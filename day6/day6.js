1989de6a4ecf10a7505db7c9b84fd8a8b0ebb477
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2022-12-24 16:22:48 -0600

    completed day 6 challenge

---
+++ day6/day6.js
@@ -0,0 +1,30 @@
+// Review Challenge 4: Taco Tray
+// - Help our chef fill a tray with tacos! 
+
+function getRandomNumberOfTacos() {
+    /*
+    Make this function return an array that contains 
+    between one and ten taco emojis 🌮
+    Use the following JavaScript concepts:
+        - Math.random()
+        - Math.floor()
+        - new Array()
+        - Array.fill()
+    */
+    let tacoArray = []
+    let numberOfTacos = Math.floor(Math.random() * 11)
+    for (let i = 0; i < numberOfTacos; i++) {
+        tacoArray.push('🌮')
+    }
+
+    return tacoArray // replace this empty tray array
+
+}
+
+function putTacosOnTray() {
+    return getRandomNumberOfTacos().map(function (taco) {
+        return `<div class="taco">${taco}</div>`
+    }).join('')
+}
+
+document.getElementById('tray').innerHTML = putTacosOnTray()
